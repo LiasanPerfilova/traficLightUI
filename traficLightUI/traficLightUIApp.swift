4c140697c3456ec4292f22dbc07199791cf54d19
LiaisanPerfilova <perliaisan@gmail.com>
2023-07-18 13:57:35 -0400

    Initial Commit

---
+++ traficLightUI/traficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  traficLightUIApp.swift
+//  traficLightUI
+//
+//  Created by Liaisan on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct traficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
